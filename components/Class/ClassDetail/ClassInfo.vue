d2322b6ad18e8b87e63793c0ea48880f5473677e
leonyangela <38250310+leonyangela@users.noreply.github.com>
2023-02-17 18:57:11 +0700

    Fix import class capitalize problem

--- components/Class/ClassDetail/ClassInfo.vue
+++ components/Class/ClassDetail/ClassInfo.vue
@@ -169,7 +169,7 @@
 import { mapGetters, mapActions } from 'vuex'
 import { staticUrl } from '@/config/api'
 import { convertToPrice } from '@/utils/price'
-import ClassLandingCategoryModal from '@/components/class/Category/ClassLandingCategoryModal'
+import ClassLandingCategoryModal from '@/components/Class/Category/ClassLandingCategoryModal'
 
 export default {
   name: 'ClassInfo',
